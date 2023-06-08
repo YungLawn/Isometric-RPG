7eb3afe628c42449e0277afe58bed47ee1035bf0
YungLawn <ccteelon@gmail.com>
2023-06-08 11:11:09 -0400

    'betterNoiseForProcGen'

--- Isometric RPG/Assets/Scripts/MapGen/MapGenerator.cs
+++ Isometric RPG/Assets/Scripts/MapGen/MapGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Tilemaps;
+using UnityEditor;
+
 
 public class MapGenerator : MonoBehaviour {
 
@@ -8,17 +10,26 @@ public class MapGenerator : MonoBehaviour {
 	public int mapHeight;
 	public float noiseScale;
 
-	public Tilemap terrainMap;
-	public IsometricRuleTile waterTile;
-	public Tile grassTile;
-	public IsometricRuleTile sandTile;
+	public int octaves;
+	[Range(0,1)]
+	public float persistance;
+	public float lacunarity;
 
-	void Start () {
-		GenerateMap();
-	}
+	public int seed;
+	public Vector2 offset;
+
+	public Tilemap terrainMap;
+	public Tile waterTile;
+	public Tile grassTile;
+	public Tile sandTile;
+	public bool autoUpdate;
+	[HideInInspector]
 
 	public void GenerateMap() {
-		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, noiseScale);
+		// float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, noiseScale);
+
+		float[,] noiseMap = Noise1.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
 
 		for (int y = 0; y < mapHeight; y++) {
 			for (int x = 0; x < mapWidth; x++) {
@@ -33,8 +44,39 @@ public class MapGenerator : MonoBehaviour {
 				}
 			}
 		}
-		// MapDisplay display = FindObjectOfType<MapDisplay> ();
-		// display.DrawNoiseMap (noiseMap);
+	}
+
+	public void SaveAssetMap() {
+        string saveName = "Map " + seed;
+        var mf = GameObject.Find("Grid");
+
+        if (mf)
+        {
+            var savePath = "Assets/" + saveName + ".prefab";
+            if (PrefabUtility.CreatePrefab(savePath,mf))
+            {
+                EditorUtility.DisplayDialog("Tilemap saved", "Your Tilemap was saved under" + savePath, "Continue");
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("Tilemap NOT saved", "An ERROR occured while trying to saveTilemap under" + savePath, "Continue");
+            }
+        }
+    }
+
+	void OnValidate() {
+		if (mapWidth < 1) {
+			mapWidth = 1;
+		}
+		if (mapHeight < 1) {
+			mapHeight = 1;
+		}
+		if (lacunarity < 1) {
+			lacunarity = 1;
+		}
+		if (octaves < 0) {
+			octaves = 0;
+		}
 	}
 	
 }
